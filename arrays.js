a4f299166af4a41413a7ed3e10fc9dcdfb6f100a
Hamilton Hudson <ehamiltonhudson@gmail.com>
2018-09-26 19:14:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 addElementToBeginningOfArray = function() {
-  chocolateBars.push("Twix");
+  chocolateBars.unshift("Twix");
 }
