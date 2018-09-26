3e452cefe1423b98c4e2892795c6754b7414fd4e
Hamilton Hudson <ehamiltonhudson@gmail.com>
2018-09-26 19:20:24 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,3 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-addElementToBeginningOfArray = function() {
-  chocolateBars.unshift("Twix");
-}
+addElementToBeginningOfArray = ["Twix",...chocolateBars]
