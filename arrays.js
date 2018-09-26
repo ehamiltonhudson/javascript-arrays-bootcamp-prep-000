fa9c2f43c98bb77ed5859097b9b459337f1ce2a8
Hamilton Hudson <ehamiltonhudson@gmail.com>
2018-09-26 20:06:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -19,6 +19,6 @@ function destructivelyAddElementToEndOfArray(array, element) {
 }
 
 function accessElementInArray(array, index) {
-  console.log(index[3])
+  console.log(index[0])
   return array
 }
