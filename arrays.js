5011f535519d9ffd4a924c7650d9287dd5940136
Hamilton Hudson <ehamiltonhudson@gmail.com>
2018-09-26 19:29:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 var myArray = ["fie", "fie", "foe", "fum"]
 
-addElementToBeginningOfArray = function() {
+function addElementToBeginningOfArray() =
   ["foo", ...myArray]
 }
 
