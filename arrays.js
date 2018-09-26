50f38ed3d55b2f90ce6f90cee3bb79d3950a6de2
Hamilton Hudson <ehamiltonhudson@gmail.com>
2018-09-26 20:31:07 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -29,6 +29,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array, element) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(0)
+  array.slice(-1)
   return array
 }
